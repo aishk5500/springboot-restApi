2602e56fc6f9fc05d3a3f6210be5b0509ee16ffa
aishk5500 <aishwaryak017@gmail.com>
2023-11-22 00:34:38 +0530

    modified CourseServiceImpl class to use the DAO to get the data

--- src/main/java/com/spring/springrest/service/CourseServiceImpl.java
+++ src/main/java/com/spring/springrest/service/CourseServiceImpl.java
@@ -1,6 +1,8 @@
 package com.spring.springrest.service;
 
+import com.spring.springrest.dao.CourseDAO;
 import com.spring.springrest.entity.Course;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -9,54 +11,60 @@ import java.util.stream.Collectors;
 
 @Service
 public class CourseServiceImpl implements CourseService{
-    List<Course> list=new ArrayList<>();
-
-    private List<Course> getList(){
-        list=new ArrayList<>();
-        list.add(new Course(145, "java","this is desc"));
-        list.add(new Course(22,"java2","desc2"));
-        return list;
-    }
+//    List<Course> list=new ArrayList<>();
+    @Autowired
+    private CourseDAO courseDAO;
+//    private List<Course> getList(){
+//        return courseDAO.findAll();
+//        list=new ArrayList<>();
+//        list.add(new Course(145, "java","this is desc"));
+//        list.add(new Course(22,"java2","desc2"));
+//        return list;
+//    }
     @Override
     public List<Course> getCourses() {
-        return getList();
+//        return getList();
+        return courseDAO.findAll();
     }
 
     @Override
     public Course getCourse(Long courseId) {
-        Course c = null;
-        list=getList();
-        for (Course course:list) {
-            if(course.getId()== courseId){
-                c=course;
-                break;
-            }
-        }
-        return c;
+//        Course c = null;
+//        list=getList();
+//        for (Course course:list) {
+//            if(course.getId()== courseId){
+//                c=course;
+//                break;
+//            }
+//        }
+//        return c;
+        return courseDAO.findById(courseId).get();
     }
 
     @Override
     public Course addCourse(Course course) {
-        getList().add(course);
-        return course;
+//        getList().add(course);
+//        return course;
+        return  courseDAO.save(course);
     }
 
     @Override
     public Course updateCourse(Course course) {
-        list=getList();
-        list.forEach(e->{
-            if(e.getId()==course.getId()){
-                e.setTitle(course.getTitle());
-                e.setDesc(course.getDesc());
-            }
-        });
-        return course;
+//        list=getList();
+//        list.forEach(e->{
+//            if(e.getId()==course.getId()){
+//                e.setTitle(course.getTitle());
+//                e.setDesc(course.getDesc());
+//            }
+//        });
+//        return course;
+       return courseDAO.save(course);
     }
 
     @Override
     public void deleteCourse(int i) {
-        list=getList();
-        list=this.list.stream().filter(e->e.getId()!=i).collect(Collectors.toList());
-
+//        list=getList();
+//        list=this.list.stream().filter(e->e.getId()!=i).collect(Collectors.toList());
+     courseDAO.deleteById((long) i);
     }
 }
